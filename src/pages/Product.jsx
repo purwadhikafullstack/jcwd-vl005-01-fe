a63b1b4dbca3ab860d0e7602062faaf2e97ab6eb
aldysprima <aldys.prima@gmail.com>
2022-06-14 08:36:46 +0700

    update mui version 5

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,4 @@
-import { Add, Remove } from "@material-ui/icons";
+import { Add, Remove } from "@mui/icons-material";
 import styled from "styled-components";
 import Announcement from "../components/Announcement";
 import Footer from "../components/Footer";
